b805c5e8578caedfd5d25b2a1af7f77a87abad71
Casper <casper.dennijs@outlook.com>
2023-02-07 12:33:52 +0100

    Update members.js

--- netlify/functions/members.js
+++ netlify/functions/members.js
@@ -2,8 +2,18 @@ import fetch from 'node-fetch';
 
 export const handler = async (event) => {
 
-    let res = await fetch(`https://whois.fdnd.nl/api/v1/members?${event.rawQuery}`)
+    let res = await fetch(`https://whois.fdnd.nl/api/v1/members`)
     let data = await res.json();
+
+    for (var i = 0; i < data.length; ++i) {
+      var memberName = data[i];
+     
+      if (memberName.members.name == event.rawQuery) {
+        let res2 = await fetch(`https://whois.fdnd.nl/api/v1/members?id=` + memberName.members.id);
+        let data2 = await res2.json();
+        break;
+      }
+    }
 
     return {
       statusCode: 200,
@@ -14,7 +24,7 @@ export const handler = async (event) => {
         'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify({
-        data: data
+        data: data2
       })
     }
   }
